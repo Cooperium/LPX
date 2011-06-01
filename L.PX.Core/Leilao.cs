22377ac1fd5e183845bf830be013ed8da55e23e0
Pablo Fonseca <pablofonseca@cooperium.com>
2011-06-01 16:01:39 -0300

    - Implemetada o corpo da classe Leilao.cs e classes relacionadas. - Implementado o corpo da classe de testes
    Signed-off-by: Pablo Fonseca <pablofonseca@cooperium.com>

---
+++ L.PX.Core/Leilao.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L.PX.Core
+{
+    public class Leilao
+    {
+        private List<Participante> participantes = new List<Participante>();
+        private List<LanceProcessado> lancesProcessados = new List<LanceProcessado>();
+
+        public decimal ValorInicial { get; private set; }
+        public Int32 NumeroDeLotes { get; private set; }
+
+        public void AddContratante(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException();
+
+            var contratante = Participante.Build(user, Papel.Contratante);
+            participantes.Add(contratante);
+        }
+
+        public Participante FindParticipante(User user)
+        {
+            return participantes.SingleOrDefault(p => p.user == user);
+        }
+
+        public void AddParticipante(User user)
+        { }
+
+        public LanceProcessado ProcessarLance(Lance lance)
+        {
+            throw new NotImplementedException();
+            OrdenarLances();
+        }
+
+        /// <summary>
+        /// Reclassifica todos os lances quanto ao status (Atendido, Parcialmente Atendido, Não Atendido) e atualiza o numero de lotes atendidos.
+        /// </summary>
+        private void OrdenarLances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LanceProcessado> ListLancesByUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class Participante
+        {
+            public User user { get; private set; }
+            public Papel papel { get; private set; }
+
+            private Participante() { }
+
+            public static Participante Build(User user, Papel papel)
+            {
+                return new Participante() { papel = papel, user = user };
+            }
+        }
+
+        public class Lance
+        {
+            public decimal Incremento { get; private set; }
+            public Int32 NumeroDeLotes { get; private set; }
+            public User Usuario { get; private set; }
+
+            private Lance() { }
+
+            public static Lance Build(Decimal incremento, int numeroDeLotes, User usuario)
+            {
+                return new Lance() { Incremento = incremento, NumeroDeLotes = numeroDeLotes, Usuario = usuario };
+            }
+        }
+
+
+        public class LanceProcessado
+        {
+            public Lance Lance { get; private set; }
+            public LanceStatus Status { get; set; }
+            public Int32 NumeroDeLotesAtendidos { get; set; }
+        }
+
+        public enum LanceStatus
+        {
+            Rejeitado,
+            Atendido
+        }
+
+        public enum Papel
+        {
+            Contratante,
+            Participante,
+        }
+
+    }
+}
---
+++ L.PX.Tests/TestLeilaoCore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using L.PX.Core;
+
+namespace L.PX.Tests
+{
+    [TestClass]
+    public class TestLeilaoCore
+    {
+        private Leilao leilao = null;
+        
+        [TestInitialize]
+        public void SetupTest1()
+        {
+  
+        
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var leilao = new Leilao();
+
+            var user1 = new User() { Email = "asd@12312.123123"};
+
+            leilao.AddContratante(user1);
+
+            Assert.IsTrue(leilao.FindParticipante(user1).papel == Leilao.Papel.Contratante);
+
+        }
+
+               [TestCleanup]
+        public void SetupTest1()
+        {
+
+        
+    }
+}
